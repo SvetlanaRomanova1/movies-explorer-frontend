0172a166d0ca6984237a02772b80c9dbbd31f866
SvetlanaRomanova1 <sromanova@team.amocrm.com>
2023-12-05 20:51:25 +0300

    Добавила active__save

--- movies-explorer-frontend/src/components/MoviesCard/MoviesCard.jsx
+++ movies-explorer-frontend/src/components/MoviesCard/MoviesCard.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './MoviesCard.css';
 
 function MoviesCard({ id, title, duration, image }) {
+    const [isActive, setIsActive] = useState(false);
+
+    const onClick = () => {
+        setIsActive(!isActive);
+    }
+
+    const saveCardClass = isActive ? 'movies-card__save' : ''
+
     return (
-        <article className="movies-card">
+        <article className={`movies-card ${saveCardClass}`}>
             <div className="movies-card__image" style={{ backgroundImage: `url(${image})` }} />
-            <button className="movies-card__button_save">Сохранить</button>
+            <button
+                onClick={onClick}
+                className="movies-card__button_save"
+            >Сохранить</button>
             <div className="movies-card__context">
                 <p className="movies-card__title">{title}</p>
                 <span className="movies-card__duration">{duration}</span>
